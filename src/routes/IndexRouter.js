3e8378213680bb39a4157a69a895393db36c669e
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-01-11 13:54:55 -0500

    config deploy

--- src/routes/IndexRouter.js
+++ src/routes/IndexRouter.js
@@ -5,11 +5,11 @@ import UserProfile from "../pages/UserProfile";
 
 const IndexRouter = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user/:username" element={<UserProfile/>}/>
-        </Routes>
-    )
+      <Routes>
+        <Route path="/torre_test/" element={<Home />} />
+        <Route path="/torre_test/user/:username" element={<UserProfile />} />
+      </Routes>
+    );
 }
 
 export default IndexRouter;
