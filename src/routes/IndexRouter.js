b5e009ff556a7dca3e4bf076ca53124cb0df7894
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-01-11 13:51:46 -0500

    config deploy

--- src/routes/IndexRouter.js
+++ src/routes/IndexRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
 import UserProfile from "../pages/UserProfile";
 
 const IndexRouter = () => {
     return (
         <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/user/:username" element={<UserProfile/>}/>
         </Routes>
     )
