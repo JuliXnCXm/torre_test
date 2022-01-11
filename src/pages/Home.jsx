a9c31e2c228712c1286303cfe8b23c4a5111dbfc
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-01-11 14:16:37 -0500

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
-            <h1>home</h1>
+            <Link>Torrenegra user</Link>
         </div>
     )
 }
