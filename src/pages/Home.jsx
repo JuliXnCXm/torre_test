db3bf8ea324c52a7c1418123b901030746776454
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-01-19 12:08:17 -0500

    initial commit second chance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
-            <Link to="/torre_test/user/torrenegra">Torrenegra user</Link>
+            <h1>home</h1>
         </div>
     )
 }
